242dc2a97a9f483d6529ef060b6acc8f77d01e0b
Lakshya <lakshyakhanna.11152005@gmail.com>
2025-03-24 03:53:57 +0530

    making db_init.py

--- backend/db_init.py
+++ backend/db_init.py
@@ -1,1 +1,11 @@
+from models import db
+from flask import Flask
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://username:password@localhost:5432/resume_db"
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+    print("Database initialized successfully.")
  
