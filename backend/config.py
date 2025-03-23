bebd3de4640a74f9b1192a040b92a0231f0f8b96
Lakshya <lakshyakhanna.11152005@gmail.com>
2025-03-24 03:52:57 +0530

    making config.py

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,15 @@
- 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_CONFIG = {
+    "POSTGRES": {
+        "dbname": os.getenv("POSTGRES_DB"),
+        "user": os.getenv("POSTGRES_USER"),
+        "password": os.getenv("POSTGRES_PASSWORD"),
+        "host": os.getenv("POSTGRES_HOST"),
+    },
+    "MONGO_URI": os.getenv("MONGO_URI"),
+}
+
